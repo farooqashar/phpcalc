7068c3f02ff91c1973132c7989f27cdf40e90436
Ashar Farooq <ashar744@gmail.com>
2021-08-03 19:55:47 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PHPCalc</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <form action="actions.php" method="GET">
+            <input>
+
+</form>
+    </body>
+
+</html>
